f240a96a6a1fb1f3c52af6efa9c6b59125bd7b3e
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-19 01:22:38 +0600

    Create arraysort.c

---
+++ Other's Practice/arraysort.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int temp;
+    int array[]={2,3,543,34,23,34,234};
+    for(int i=0; i<7; i++)
+    {
+        if( temp<array[i])
+        {
+
+        }
+         temp= array[i];
+        
+    }
+}
