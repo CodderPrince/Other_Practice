e03fdf2a65d327d59173d3915601a125d7b23c82
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-19 01:26:14 +0600

    Create TRANSPOSE.cpp

---
+++ Other's Practice/TRANSPOSE.cpp
@@ -0,0 +1,19 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int mat[3][3];
+    int i,j;
+    cout<<"Enter a matrix:"<<endl;
+    for(i=0; i<3; i++)
+    {
+        for(j=0; j<3; j++)
+        {
+            scanf("%d",&mat[i][j]);
+        }
+    }
+
+    
+}
