ac88fab715f1668cf0ede6556cdb82e73af55d72
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-19 01:22:11 +0600

    Create 55.c

---
+++ Other's Practice/55.c
@@ -0,0 +1,15 @@
+#include int main()
+{
+int N, x, pos = 0, neg = 0;
+printf("Enter the Numbers limit = ");
+scanf("%d", &N);
+printf("Enter %d numbers\n", N);
+while(N)
+{
+ scanf("%d", &x);
+if(x > 0) pos++;
+else if(x neg++;
+else break;
+N--;
+}...
+
