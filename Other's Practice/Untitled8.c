4e42a919a728cec93c7d3e7b22deaae476c002a8
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-19 01:26:35 +0600

    Create Untitled8.c

---
+++ Other's Practice/Untitled8.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int days,years,weeks,restdays;
+printf("Enter the total days: ");
+scanf("%d",&days);
+years= days/365;
+printf("Total %d years\n", years);
+weeks=  (days-(years*365))/7;
+printf("Total %d weeks\n",weeks);
+restdays=days-(weeks*7);
+printf("Total %d restdays\n",restdays);
+return 0;
+}
