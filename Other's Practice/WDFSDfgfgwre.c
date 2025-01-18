ee92971b68f68abd792592369b60f716f01cc0e2
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-19 01:26:41 +0600

    Create WDFSDfgfgwre.c

---
+++ Other's Practice/WDFSDfgfgwre.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int num=1, res, i, j;
+    printf("\t\tTable from 1 to 10\n\n");
+    for(i=0; i<10; i++)
+    {
+        printf("Table of %d = ", num);
+        for(j=1; j<=10; j++)
+        {
+            res = num*j;
+            printf("%d  ", res);
+        }
+        printf("\n");
+        num++;
+    }
+
+    return 0;
+}
