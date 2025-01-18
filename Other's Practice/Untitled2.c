c629551fd2c19e94b0f1aa7c0fb2a12558e8f7ea
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-19 01:26:23 +0600

    Create Untitled2.c

---
+++ Other's Practice/Untitled2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    char letter;
+printf("Enter the small letter alphabet:\n");
+scanf("%c",&letter);
+printf("Capital version is : %c\n",letter-32);
+
+return 0;
+}
